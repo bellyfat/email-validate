d0f3a33ba9ffee681a9b54ba8baf8d1dc53eb6d2
Eddie <hi@dev.cool>
2021-04-06 22:17:11 +0800

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='email-validate',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(exclude=['tests']),
     install_requires=['dnspython~=2.0', 'idna~=3.0', 'filelock~=3.0'],
     author='CPILab',
